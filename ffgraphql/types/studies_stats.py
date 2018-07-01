e48e6a5765e62dea537264123455a7333b177035
Adamos Kyriakou <somada141@gmail.com>
2018-07-01 15:21:46 +1000

    `studies_stats.py`: Updated the `TypeStudiesStats` class and added a new `count_studies_by_facility` resource and its corresponding resolver which calculates the number of studies by facility given study IDs.

--- ffgraphql/types/studies_stats.py
+++ ffgraphql/types/studies_stats.py
@@ -10,6 +10,10 @@ from fform.orm_ct import Location as LocationModel
 from fform.orm_ct import Facility as FacilityModel
 from fform.orm_ct import OverallStatusType as OverallStatusEnum
 
+from ffgraphql.types.facilities import FacilityType
+from ffgraphql.utils import extract_requested_fields
+from ffgraphql.utils import apply_requested_fields
+
 
 class TypeCountStudiesCountry(graphene.ObjectType):
     """Graphene type representing a single result of an aggregation operation
@@ -57,6 +61,14 @@ class TypeStudiesStats(graphene.ObjectType):
 
     count_studies_by_overall_status = graphene.List(
         of_type=TypeCountStudiesOverallStatus,
+        study_ids=graphene.Argument(
+            type=graphene.List(of_type=graphene.Int),
+            required=True
+        ),
+    )
+
+    count_studies_by_facility = graphene.List(
+        of_type=TypeCountStudiesFacility,
         study_ids=graphene.Argument(
             type=graphene.List(of_type=graphene.Int),
             required=True
@@ -160,6 +172,75 @@ class TypeStudiesStats(graphene.ObjectType):
         objs = [
             TypeCountStudiesOverallStatus(
                 overall_status=OverallStatusEnum(result[0]).value,
+                count_studies=result[1]
+            ) for result in results
+        ]
+
+        return objs
+
+    @staticmethod
+    def resolve_count_studies_by_facility(
+        args: dict,
+        info: graphene.ResolveInfo,
+        study_ids: List[int],
+    ) -> List[TypeCountStudiesFacility]:
+        """Creates a list of `TypeCountStudiesFacility` objects with the number
+        of clinical-trial studies per facility.
+
+        Args:
+            args (dict): The resolver arguments.
+            info (graphene.ResolveInfo): The resolver info.
+            study_ids (List[int]): A list of Study IDs.
+
+        Returns:
+             list[TypeCountStudiesFacility]: The list of
+                `TypeCountStudiesFacility` objects with the results of the
+                aggregation.
+        """
+
+        # Retrieve the session out of the context as the `get_query` method
+        # automatically selects the model.
+        session = info.context.get("session")  # type: sqlalchemy.orm.Session
+
+        # Define the `COUNT(studies.study_id)` function.
+        func_count_studies = sqlalchemy_func.count(StudyModel.study_id)
+
+        # Query out the count of studies by facility.
+        query = session.query(
+            FacilityModel,
+            func_count_studies,
+        )  # type: sqlalchemy.orm.Query
+        query = query.join(StudyModel.locations)
+        query = query.filter(StudyModel.study_id.in_(study_ids))
+        query = query.join(
+            FacilityModel,
+            LocationModel.facility_id == FacilityModel.facility_id
+        )
+        # Group by study facility.
+        query = query.group_by(FacilityModel.facility_id)
+
+        # Extract the fields requested in the GraphQL query.
+        fields = extract_requested_fields(
+            info=info,
+            fields=info.field_asts,
+            do_convert_to_snake_case=True,
+        )["count_studies_by_facility"]["facility"]
+
+        # Limit query to `Facility` fields requested in the GraphQL query.
+        query = apply_requested_fields(
+            info=info,
+            query=query,
+            orm_class=FacilityModel,
+            fields={"facility": fields},
+        )
+
+        results = query.all()
+
+        # Wrap the results of the aggregation in `TypeCountStudiesFacility`
+        # objects.
+        objs = [
+            TypeCountStudiesFacility(
+                facility=result[0],
                 count_studies=result[1]
             ) for result in results
         ]
