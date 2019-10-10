1212b1cd8ef30fe3f131ca8b89b4a036e5ac9ce8
Adamos Kyriakou <somada141@gmail.com>
2019-10-10 11:18:19 +1030

    [ch1398]: Added a default study-count ordering.
    [ch1398]: Updated the `resolve_count_studies_by_facility` method of the `TypeStudiesStats` class and added a default study-count ordering when no other order has been defined.

--- ffgraphql/types/studies_stats.py
+++ ffgraphql/types/studies_stats.py
@@ -768,6 +768,12 @@ class TypeStudiesStats(graphene.ObjectType):
                 query = query.order_by(
                     getattr(ModelFacilityCanonical, order_by).asc(),
                 )
+        # Order by the number of studies.
+        else:
+            if order == TypeEnumOrder.ASC.value:
+                query = query.order_by(func_count_studies.asc())
+            else:
+                query = query.order_by(func_count_studies.desc())
 
         # Apply offset (if defined).
         if offset:
