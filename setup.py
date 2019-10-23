219e78daf83cb011beb6538bf3cdbed0223da9ce
Adamos Kyriakou <somada141@gmail.com>
2019-10-23 12:53:25 +1030

    Bumped version and updated `CHANGELOG.md`.

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/somada141/ffgraphql',
-    version='0.20.2',
+    version='0.21.0',
     zip_safe=False,
 )
